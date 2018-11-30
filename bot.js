97850032ca98e9a7ded5b05f5282646be0678597
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-11-30 20:11:33 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,19 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var prefix = 'os';
+
+
+client.on('message', message => {
+	if (!message.channel.guild) return;
+	if (message.author.bot) return;
+    let args = message.content.split(" ").slice(1).join(" ");
+    if (message.content.startsWith(prefix + 'bc')) {
+	if(!args[0]) return message.channel.send(`Error: You have to write a message .`);
+	message.channel.send(`Sending message to ${message.guild.memberCount} members ..`);
+            message.guild.members.forEach(m => {
+	m.send(`${args}`)
+	    });
+	}
+});
 
 client1.login(process.env.BOT_TOKEN);
