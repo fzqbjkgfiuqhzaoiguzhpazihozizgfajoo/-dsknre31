66b759837cbf2adf49fb88fc5fc9b189072e8764
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-11-30 21:26:18 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,6 +58,37 @@ client.on('message', message => {
 });
 
 //           Music 1           //
+
+client1.on('message', message => {
+
+    if (!message.channel.guild) return;
+    if (message.author.bot) return;
+        let args = message.content.slice(prefix.length).trim().split(' ');
+        if (message.content.startsWith(prefix + `search`)) {
+    search(args.join(' '), function(err, res) {
+       if(err) return message.channel.send(`Sorry, something went wrong`);
+       
+       let videos = res.videos.slice(0 , 5);
+       
+       let resp = '';
+       for (var i in videos) {
+        resp += `\n**[${parseInt(i)+1}]:** \` ${videos[i].title}\`. `;
+        
+       }
+       resp += `\n**Choose a number between **\`1-${videos.length}\`.`;
+       
+       
+       message.channel.send(resp);
+       
+       const filter = m => !isNaN(m.content) && m.content < videos.length+1 && m.content > 0;
+       const collector = message.channel.createMessageCollector(filter);
+       
+       collector.videos = videos;
+
+       
+    });
+}
+});
 //leave
 client1.on('message', message => {
     if (!message.channel.guild) return;
