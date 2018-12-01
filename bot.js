f1f75bd4eb44264b3c1f651baf647a3ccbab80ec
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-12-01 18:17:42 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix1 = 'os-';
+var prefix = 'os-';
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
@@ -46,7 +46,7 @@ client.on('message', message => {
 	if (!message.channel.guild) return;
 	if (message.author.bot) return;
     let args = message.content.split(" ").slice(1).join(" ");
-    if (message.content.startsWith(prefix1 + 'bc')) {
+    if (message.content.startsWith(prefix + 'bc')) {
 	if(!args[0]) return message.channel.send(`Error: You have to write a message .`);
 	message.channel.send(`Sending message to ${message.guild.memberCount} members ..`);
             message.guild.members.forEach(m => {
